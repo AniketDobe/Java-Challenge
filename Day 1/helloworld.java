1964d04441edca680be8453cb5a3c57af8619f4d
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-09 12:07:09 +0530

    Update helloworld.java

--- Day 1/helloworld.java
+++ Day 1/helloworld.java
@@ -1,5 +1,9 @@
 public class helloworld{
     public static void main(String args[]){
+        // comment
         System.out.println("Hello World!");
     }
 }
+
+// AddTwoNumber    -->     Pascal naming convention for class name
+// addTwoNumber    -->     Camelcase naming convention for functions/methods names
