8150900981d948746c4e20221c39ae09e1a6c66f
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-09 14:25:45 +0530

    Add files via upload

---
+++ Day 1/addition.java
@@ -0,0 +1,9 @@
+public class addition{
+    public static void main(String args[]){
+        int num1=4;
+        int num2=5;
+        int num3=6;
+        int sum = num1+num2+num3;
+        System.out.println("Addition of three number is: "+sum);
+    }
+}
