a8edc24a97e94b0c0efde2db93a822b8e405659c
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-11 22:43:30 +0530

    Add files via upload

---
+++ Day 2/literals.java
@@ -0,0 +1,15 @@
+public class literals{
+    public static void main(String args[]){
+        byte age = 34;
+        char ch = 'A';
+        float f = 5.6f;
+        double d = 4.44;
+        System.out.println(age);
+        String str = "Aniket";
+        System.out.println(str);
+    }
+}
+
+
+// constant values are called literals
+// keywords are the reserved words in java 
