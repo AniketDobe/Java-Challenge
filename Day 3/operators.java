34714518f118f771b6f197b3dfe9123302833cb3
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-12 18:53:29 +0530

    Add files via upload

---
+++ Day 3/operators.java
@@ -0,0 +1,18 @@
+public class operators{
+    public static void main(String args[]){
+        int a = 6;
+        int b = 5;
+        // Arithmetic operators +, -, /, *, %
+        System.out.println(a+b);
+
+        // Assignment operators =, +=, -=, /=, *=, %=
+        System.out.println(a+=b);
+
+        // Comparision operators >, <, ==, , <=, >=
+        System.out.println(a>b);
+
+        // Logical operators &&, ||, !
+        System.out.println(a>b && b>1);
+        
+    }
+}
