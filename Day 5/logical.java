2d1be696cf173c570fd5bf95ea41929ee122f15d
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-14 19:07:46 +0530

    Add files via upload

---
+++ Day 5/logical.java
@@ -0,0 +1,31 @@
+public class logical {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        // System.out.println("logical and");
+        // if(a && b){
+        //     System.out.println("Yes");
+        // }
+        // else{
+        //     System.out.println("No");
+        // }
+
+        // System.out.println("logical or");
+        // if(a || b){
+        //     System.out.println("Yes");
+        // }
+        // else{
+        //     System.out.println("No");
+        // }
+
+        System.out.println("logical not");
+        System.out.println("Not (a) is "+!a);
+        System.out.println("Not (b) is "+!b);
+        
+    }
+}
+
+// logical operators 
+// &&      and
+// ||      or
+// !       not
