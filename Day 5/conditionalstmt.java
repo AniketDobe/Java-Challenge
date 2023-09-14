ea1911f8751ad3eda5d420aff8f4462010c8a999
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-14 18:59:09 +0530

    Add files via upload

---
+++ Day 5/conditionalstmt.java
@@ -0,0 +1,11 @@
+public class conditionalstmt{
+    public static void main(String[] args) {
+        int age = 6;
+        if(age>18){
+            System.out.println("Yes you can drive!");
+        }
+        else{
+            System.out.println("No boy you can not drive yet!");
+        }
+    }
+}
