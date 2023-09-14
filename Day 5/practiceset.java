e5074d7fd42464d4ab7e2dd3b80c5419c6a2a364
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-14 19:48:42 +0530

    Add files via upload

---
+++ Day 5/practiceset.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class practiceset {
+    public static void main(String[] args) {
+//        Question 1:
+//        int a = 11;
+//        if(a=11){
+//
+//        }
+
+        // Question 2
+//        byte m1, m2, m3;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter your marks in Physics");
+//        m1 = sc.nextByte();
+//
+//        System.out.println("Enter your marks in Chemistry");
+//        m2= sc.nextByte();
+//
+//        System.out.println("Enter your marks in Mathematics");
+//        m3 = sc.nextByte();
+//        float avg = (m1+m2+m3)/3.0f;
+//        System.out.println("Your Overall percentage is: " + avg);
+//        if(avg>=40 && m1>=33 && m2>=33 && m3>=33){
+//            System.out.println("Congratulations, You have been promoted");
+//        }
+//        else{
+//            System.out.println("Sorry, You have not been promoted! Please try again.");
+//        }
+
+
+//        Question 3:
+
+//        Scanner sc = new Scanner(System.in);
+//        int day = sc.nextInt();
+//
+//        switch (day){
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//        }
+
+//        Question 4
+//        Scanner sc = new Scanner(System.in);
+//        String website = sc.next();
+//        if(website.endsWith(".org")){
+//            System.out.println("This is an organizational website");
+//        }
+//        else if(website.endsWith(".com")){
+//            System.out.println("This is a Commercial website");
+//        }
+//        else if(website.endsWith(".in")){
+//            System.out.println("This is an Indian website");
+//        }
+
+    Random r = new Random();
+    int a = r.nextInt();
+        System.out.println(a);
+
+    }
+}
