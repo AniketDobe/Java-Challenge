690a49ebf4c0499f9b136e0af6cf3b14f97fc6e2
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-15 21:47:13 +0530

    Add files via upload

---
+++ Day 6/forloop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class forloop {
+    public static void main(String[] args) {
+        // for(int i=1; i<=10; i++){
+        //     System.out.println(i);
+        // }
+
+        // System.out.println("Enter number for odd no. generations");
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+
+        // for(int i=0; i<a; i++){
+        //     System.out.println(2*i+1);
+        // }
+
+        for(int i=5; i>=0; i--){
+            System.out.println(i);
+        }
+    }
+}
+
+
+// initialization; condition; increament
