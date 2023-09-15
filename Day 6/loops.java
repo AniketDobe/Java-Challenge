634c1729bd71ea0bec88e8961d52e6f0e72011c9
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-15 21:24:34 +0530

    Add files via upload

---
+++ Day 6/loops.java
@@ -0,0 +1,19 @@
+public class loops{
+    public static void main(String[] args) {
+
+        // System.out.println(1);
+        // System.out.println(2);
+        // System.out.println(3);
+       
+       int i = 1;
+       while(i<=3){
+        System.out.println(i);
+        i++;
+       } 
+       System.out.println("Finish while loop");
+
+       while(true){
+        System.out.println("I am infinite loop");
+       }
+    }
+}
