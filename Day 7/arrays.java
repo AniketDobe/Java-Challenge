0b77738ac0b99b861321fd3a1b83d9a1f347fe0b
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-09-16 19:28:57 +0530

    Add files via upload

---
+++ Day 7/arrays.java
@@ -0,0 +1,28 @@
+public class arrays{
+    public static void main(String[] args) {
+        // if 5 students
+        // 1. create 5 variables
+        // 2. create array
+
+        // three wys to create array in java
+        //1.    int []marks = new int[5];       //declaration & memory allocation
+
+        // declaration and mamory allocation
+        //2.    int []marks;
+        //      marks = new int[5];
+        // initialization
+        // marks[0] = 99;
+        // marks[1] = 85;
+        // marks[2] = 25;
+        // marks[3] = 85;
+        // marks[4] = 65;
+        // System.out.println(marks[4]);
+
+        // 3. declaration, memory allocation & initialization
+        int [] marks = {12,15,87,85,87};
+        System.out.println(marks[4]);
+
+
+
+    }
+}
